f7764b021f12aca989d070227db3477e4572f5be
sweetienianyu <475493955@qq.com>
2021-02-10 11:20:42 +0800

    ast

--- src/lexer/TokenType.js
+++ src/lexer/TokenType.js
@@ -1,12 +1,12 @@
 const Enum = require('../common/Enum')
 
 module.exports = {
-  KEYWORD: new Enum('KEYWORD', 1),
-  VARIABLE: new Enum('VARIABLE', 1),
-  OPERATOR: new Enum('OPERATOR', 1),
-  BRACKET: new Enum('BRACKET', 1),
-  INTEGER: new Enum('INTEGER', 1),
-  FLOAT: new Enum('FLOAT', 1),
-  BOLEAN: new Enum('BOLEAN', 1),
+    KEYWORD : new Enum("KEYWORD", 1),
+    VARIABLE : new Enum("VARIABLE", 2),
+    OPERATOR : new Enum("OPERATOR", 3),
+    BRACKET: new Enum("BRACKET", 4),
+    INTEGER: new Enum("INTEGER", 5),
+    FLOAT: new Enum("FLOAT", 6),
+    BOOLEAN : new Enum("BOOLEAN", 7),
+    STRING : new Enum("STRING", 8)
 }
-
